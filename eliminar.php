9fc0f0c052e6bcf7f577698f96cf131c24a86714
Erika <eriikangel1502@gmail.com>
2020-11-26 18:57:23 -0500

    problema agregado en eliminar

--- eliminar.php
+++ eliminar.php
@@ -4,13 +4,5 @@ $fecha = $_POST['fecha'];
 $sql = "delete from agendar where fecha ='" . $fecha . "'";
 $resultado = mysqli_query($conexion, $sql) or die ('Error al eliminar cita');
 echo("Cita Eliminada");
-
-if(@mysqli_insert_id){
-
-    return 1;
-}else{
-
-    return 0;
-}
 echo("<br><br><a href='index.html> Regresar a Agendar Cita </a>");
 ?>
